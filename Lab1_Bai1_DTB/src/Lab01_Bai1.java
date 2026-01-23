d2cec4a191437467ac4da76e89242318271c316a
Nguyễn Trường Sơn <son.0778713447@gmail.com>
2026-01-23 19:43:32 +0700

    Update Lab01_Bai1.java

--- Lab1_Bai1_DTB/src/Lab01_Bai1.java
+++ Lab1_Bai1_DTB/src/Lab01_Bai1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Lab01 {
+public class Lab01_Bai1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Nhap ho va ten: ");
